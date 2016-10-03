ba07e9835a754797809887fa9f8de2bf18861f9a
Matthew Gamble <git@matthewgamble.net>
2016-10-03 23:42:01 +1100

    Use one thread per notifier when retrying forever
    This aims to ensure that at least some of the notifiers will eventually
    succeed.

--- journald_notify/notifiers/notifier_group.py
+++ journald_notify/notifiers/notifier_group.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 from .notifier import Notifier
 
 
@@ -13,7 +14,19 @@ class NotifierGroup(Notifier):
 
     def notify(self, title, message, retry_forever=False):
         for notifier in self._notifiers:
-            try:
-                notifier.notify(title, message, retry_forever)
-            except BaseException as e:
-                self._logger.warn("Fatal error while running {0} notifier: {1}".format(notifier.__class__.__name__, e))
+            if retry_forever:
+                # These notifications could go on forever and never actually trigger, so it's
+                # important that they each run in their own thread. This at least gives some of
+                # the notifiers a chance to succeed.
+                # This isn't necessary when not retrying forever, as notifiers should always
+                # time out after three failed attempts.
+                notifier_thread = threading.Thread(target=self._notify, args=(notifier, title, message, retry_forever), daemon=True)
+                notifier_thread.start()
+            else:
+                self._notify(notifier, title, message, retry_forever)
+
+    def _notify(self, notifier, title, message, retry_forever):
+        try:
+            notifier.notify(title, message, retry_forever)
+        except BaseException as e:
+            self._logger.warn("Fatal error while running {0} notifier: {1}".format(notifier.__class__.__name__, e))
