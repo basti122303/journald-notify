a481936d879a44f59bb872261c92577d6b615acb
Matthew Gamble <git@matthewgamble.net>
2016-10-07 08:37:07 +1100

    Add ability to only run notifications upon certain conditions
    All capturing groups in filter regexes must now be named. This is
    necessary to make conditions work effectively.

--- journald_notify/filter.py
+++ journald_notify/filter.py
@@ -1,13 +1,15 @@
 import re
+from ._config import ConfigError
 
 
 class Filter(object):
-    def __init__(self, regex, title, body="", services=[], notifiers=[]):
+    def __init__(self, regex, title, body="", services=[], notifiers=[], conditions=[]):
         self._regex = re.compile(regex)
         self._title = title
         self._body = body
         self._services = services
         self.notifiers = notifiers
+        self._conditions = conditions
 
     def match(self, entry, service):
         if self._services and service not in self._services:
@@ -15,12 +17,35 @@ class Filter(object):
         m = self._regex.search(entry)
         if not m:
             return None
+        matched_values = m.groupdict()
+
+        if self._conditions:
+            if not self._check_conditions(matched_values):
+                return None
 
         def match_resolver():
-            title = self._title.format(*m.groups())
-            body = self._body.format(*m.groups())
+            title = self._title.format(**matched_values)
+            body = self._body.format(**matched_values)
             return title, body
         return match_resolver
+
+    def _check_conditions(self, matched_values):
+        for key, value in matched_values.items():
+            if key not in self._conditions:
+                continue
+            condition_type = self._conditions[key]["type"]
+            if condition_type == "exclude":
+                for exclude in self._condition[key]["exclude"]:
+                    if re.search(exclude, value):
+                        return False
+            elif condition_type == "include":
+                for include in self._condition[key]["include"]:
+                    if re.search(include, value):
+                        return False
+            else:
+                raise ConfigError("Unknown condition type specified: {0}".format(condition_type))
+
+        return True
 
 
 def create_filters(filter_config):
@@ -31,6 +56,7 @@ def create_filters(filter_config):
             _filter["title"],
             _filter.get("body", ""),
             _filter.get("services", []),
-            _filter.get("notifiers", [])
+            _filter.get("notifiers", []),
+            _filter.get("conditions", [])
         ))
     return filters
